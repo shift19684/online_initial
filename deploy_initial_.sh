aef356f247c497d45bcdf95468eb3443242cf588
shift19684 <Srikomala_Social_Media@outlook.com>
2026-07-08 14:57:31 +0900

    Add files via upload

---
+++ deploy_initial_.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Assign the path to a variable
+sudo cp /home/sudox/online_initial/initial_.sh /home/sudox/bangs/initial_.sh
+sudo chmod +x /home/sudox/bangs/initial_.sh
+
+sudo cp /home/sudox/online_initial/int_date.sh /home/sudox/bangs/int_date.sh
+sudo chmod +x /home/sudox/bangs/int_date.sh
+
+sudo cp /home/sudox/online_initial/int_date_zero_.sh /home/sudox/bangs/int_date_zero_.sh
+sudo chmod +x /home/sudox/bangs/int_date_zero_.sh
+
+sudo cp /home/sudox/online_initial/startup.sh /home/sudox/startup/startup.sh
+sudo chmod +x /home/sudox/startup/startup.sh
+
+sudo cp /home/sudox/online_initial/startup_copy_.sh /home/sudox/startup/startup_copy_.sh
+sudo chmod +x /home/sudox/startup/startup_copy_.sh
+
+sudo rm -rf /home/sudox/online_initial/
